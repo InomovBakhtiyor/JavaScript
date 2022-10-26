531c91ec68a0ba66f3ae84104ec9fc607a60df35
InomovBakhtiyor <91469886+InomovBakhtiyor@users.noreply.github.com>
2022-10-26 15:03:40 +0900

    lesson-1

---
+++ main/js/script.js
@@ -0,0 +1,21 @@
+// LESSON---1
+
+alert("Bu alert");
+
+let number = 10;
+const borderColor = "red";
+console.log(number);
+
+number = 20;
+console.log(number);
+
+const person = {
+    name: "Samar",
+};
+
+person.name = "Bakhtiyor";
+console.log(person);
+
+// Old versions
+
+var clientName = "Bakhtiyor";
