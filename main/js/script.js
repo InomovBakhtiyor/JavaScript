4aa00296ab8bd9890ae196fd7d5070bbb5c3ba25
InomovBakhtiyor <91469886+InomovBakhtiyor@users.noreply.github.com>
2023-01-24 23:00:13 +0900

    boshlandi

--- main/js/script.js
+++ main/js/script.js
@@ -813,6 +813,7 @@ logger(...calc);
 
 //----------------------------------------------------------------------------------------------------------------
 
+/*
 // LESSON-18       OOP asoslari (Obyektga yo'naltirilgan dasturlash)
 
 'use strict';
@@ -834,4 +835,78 @@ Object.setPrototypeOf(gm, car);
 console.log(gm);
 
 const bmw = Object.create(car);
+
 console.log(bmw);
+
+*/
+
+//----------------------------------------------------------------------------------------------------------------
+
+// LESSON-20       Amaliyot(uyga vazifa) OOP
+'use strict';
+
+const seriesDB = {
+    count: 0,
+    series: {},
+    actors: {},
+    genres: [],
+    private: false,
+    start: function () {
+        seriesDB.count = +prompt("Nechta serial ko'rdingiz?", "");
+
+        while(seriesDB.count == "" || 
+             seriesDB.count == null || 
+             isNaN(seriesDB.count)) {
+                   seriesDB.count = +prompt("Nechta serial ko'rdingiz?", "");
+        }
+    },
+    rememberMySeries:function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Oxirgi ko'rgan serialingiz"),
+                  b = prompt("Nechi baho berasiz?");  
+                  
+            if (a != null && b != null && a != "" && b != "") {
+                seriesDB.series[a] = b;
+                console.log("done");
+            } else {
+                console.log("error");
+                i--;
+            }
+        }
+    },
+    detectLevelSeries: function () {
+        if (seriesDB.count < 5) {
+            console.log("Siz kam serial ko'ripsiz");
+        } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+            console.log("Siz klassik tomoshabin ekansiz");
+        } else if (seriesDB.count >= 10) {
+            console.log("siz serialchi zvezda ekansiz");
+        } else {
+            console.log("Error");
+        }
+    },
+    showDb: function() {
+        if(!seriesDB.private) {
+            console.log(seriesDB);
+        }
+    },
+    visibleDB: function () {
+        if (seriesDB.private) {
+            seriesDB.private = false;
+        } else {
+            seriesDB.private = true;
+        }
+    },
+    writeGenres: function () {
+        for (let i = 0; i < 3; i++) {
+            const genre = prompt(`Yaxshi ko'rgan janringiz? ${i+1}`, '');
+            if (genre === '' || genre === null) {
+                console.log("Siz noto'g'ri ma'lumot kiritdingiz!");
+                i--;
+            } else {
+                seriesDB.genres[i] = genre;
+            }
+            seriesDB.genres[i] = genre;
+        }
+    },
+};
